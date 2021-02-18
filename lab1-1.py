d00961ab8ed22f60223f94c427dcb3eac9367a50
dgomilko <digomilko@gmail.com>
2021-02-18 12:34:46 +0200

    labs 1.1-1.2

---
+++ lab1-1.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import signalGenerator
+import calc
+import complexity
+
+HARMONICS = 12
+MAX_FREQUENCY = 2400
+DISCRETE_CALLS = 1024
+COMPLEXITY_COUNT_LOOPS = 200
+
+signal = signalGenerator.createSignal(
+  HARMONICS,
+  MAX_FREQUENCY,
+  DISCRETE_CALLS
+)
+
+mean = calc.mean(signal)
+variance = calc.variance(signal)
+print('Mean: ', mean)
+print('Variance: ', variance)
+
+harmonics, time = complexity.getGenerationComplexity(
+  COMPLEXITY_COUNT_LOOPS,
+  MAX_FREQUENCY,
+  DISCRETE_CALLS
+)
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Lab 1.1')
+
+axs[0].plot(signal)
+axs[0].axhline(y=mean, color='r')
+axs[0].set_title('Generated signal and average value')
+axs[0].set(xlabel='time', ylabel='generated signal')
+
+axs[1].plot(harmonics, time)
+axs[1].set_title('Signal generation complexity')
+axs[1].set(xlabel='n', ylabel='elapsed time (seconds)')
+
+plt.show()
+fig.savefig('graphs/lab1-1.png')
---
+++ signalGenerator.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+def createSignal(harmonics, maxFrequency, calls):
+  sumsArr = [0] * calls
+  frequencyStep = maxFrequency / harmonics
+  for i in range(harmonics):
+    frequency = frequencyStep * (i + 1)
+    amplitude = random.random()
+    phase = random.random()
+    for t in range(calls):
+      sumsArr[t] += amplitude * math.sin(frequency * t + phase)
+  return sumsArr
+  
